da838aeb3b199186ae094f36277938978e44718e
Alex1934 <alez1934@yandex.ru>
2025-10-16 17:29:28 +0300

    изменить: точку входа в программу, но она потом поменяется

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from parser import get_random_image_url, download_image, get_random_text
+from sender import send_image
 from image_editor import edit_image
-from time import sleep
+from logger import logger
 
 def main():
-  edit_image()
+  try:
+    text = get_random_text()
+    logger.info(text)
+    image = download_image()
+    resulting_image = edit_image(image, text)
+    logger.info(resulting_image)
+    send_image(resulting_image)    
+  except Exception as e:
+    logger.error(f"An exception was thrown {e}")
 
 if __name__ == "__main__":
-  while True:
-    try:
-      main()
-      sleep(1800)
-    except Exception as e:
-      print(e)
-      continue
+  main()
